de592837aad1ee6890fe2a926d465a1e91dfd8a2
Huda Ridwan <kerupuk.lovers@gmail.com>
2017-10-05 10:34:03 +0700

    Finished level0 level1 and level2

---
+++ index.js
@@ -0,0 +1,26 @@
+# Level 0
+
+let number1 = 5;
+let number2 = 10; 
+if (number1 < number2) {
+	console.log(number1 + 'lebih kecil dari' + number2);
+} else {
+	console.log(number1 + 'lebih besar dari' + number2);
+}
+
+# Level 1
+
+let makan = 15
+
+let makan1 = 9;
+let makan2 = 12;
+
+if (makan < makan1) {
+	console.log('Saya sedang sarapan')
+} else if (makan < makan2 ) {
+	console.log('Saya sedang makan siang')
+} else {
+	console.log('Saya Makan malam')
+}
+
+# Level 2
